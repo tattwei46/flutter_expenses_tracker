7d20797c3c9e3c1fdeec81613fe2648e1909b896
DavidCheah <tattwei46@gmail.com>
2018-07-28 22:10:09 +0800

    FAB to show dialog for entering item and amount

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'dart:async';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -13,87 +11,143 @@ class MyApp extends StatelessWidget{
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(),
-          );
-        }
-      }
-      
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> { 
-  int _total = 0;
-  var _now = new DateFormat.yMMMd().format(new DateTime.now());
-
-  void _addTotalExpenses() {
-    setState(() {
-      _total++;
-    });
-  }
-
-   void showAddExpensesDialog({ BuildContext context, Widget child }) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => child,
+      home: new MyHomePage(title: 'Expenses Tracker'),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List _items = [];
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Expenses Tracker"),
+        title: new Text(widget.title),
         backgroundColor: Colors.pinkAccent,
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              _now,
-              style: TextStyle(
-                fontSize: 22.0,
-                fontWeight: FontWeight.w300
-              ),
-            ),
-            new Text(
-              "\$$_total",
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.w300
-              ),),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-              showAddExpensesDialog(
-                context: context,
-                child: new SimpleDialog(
-                  title: const Text(
-                    'Add New Expenses'),
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: 'Expense Item'
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )
-              );
-            },
+      body: new ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (BuildContext context, int position) {
+          //return new ListItem(_items[position]);
+          return getRow(position);
+        }),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add Item',
+        backgroundColor: Colors.pinkAccent,
         child: new Icon(Icons.add),
-        backgroundColor: Colors.pinkAccent,
+        onPressed: () => _openDialogAddItem(),
       ),
     );
+  }
+
+  Widget getRow(int position) {
+    return new FlatButton(
+      child: new ListTile(
+        title: new Text(_items[position].text),
+        trailing: new Text(_items[position].number.toString()),
+      ),
+      onPressed: () {
+        setState(() {
+          _items.removeAt(position);
+        });
+      },
+    );
+  }
+
+  Future _openDialogAddItem() async {
+    ModelData data = await Navigator.of(context).push(
+      new MaterialPageRoute<ModelData>(
+        builder: (BuildContext context) {
+          return new DialogAddItem();
+        },
+        fullscreenDialog: true));
+
+    setState(() {
+      _items.add(data);
+    });
+  }
+}
+
+class DialogAddItem extends StatefulWidget {
+  @override
+  _DialogAddItemState createState() => new _DialogAddItemState();
+}
+
+class _DialogAddItemState extends State<DialogAddItem> {
+  bool _canSave = false;
+  ModelData _data = new ModelData.empty();
+
+  void _setCanSave(bool save) {
+    if (save != _canSave)
+      setState(() => _canSave = save);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return new Scaffold(
+      appBar: new AppBar(
+          title: const Text('Add New Expenses'),
+          backgroundColor: Colors.pinkAccent,
+          actions: <Widget> [
+            new FlatButton(
+                child: new Text('ADD', style: theme.textTheme.body1.copyWith(color: _canSave ? Colors.white : new Color.fromRGBO(255, 255, 255, 0.5))),
+                onPressed: _canSave ? () { Navigator.of(context).pop(_data); } : null
+            )
+          ]
+      ),
+      body: new Form(
+        child: new ListView(
+          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+          children: <Widget>[
+            new TextField(
+              decoration: const InputDecoration(
+                labelText: "Tap to enter item",
+              ),
+              onChanged: (String value) {
+                _data.text = value;
+                _setCanSave(value.isNotEmpty);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0,20.0,0.0,0.0),
+              child: new TextField(
+                decoration: const InputDecoration(
+                  labelText: "Tap to enter amount",
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (String value) {
+                  _data.number = int.parse(value);
+                  _setCanSave(value.isNotEmpty);
+                },
+              ),
+            )     
+          ].toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class ModelData
+{
+  String text;
+  int number;
+
+  ModelData(this.text, this.number);
+
+  ModelData.empty() {
+    text = "";
+    number = 0;
   }
 }
